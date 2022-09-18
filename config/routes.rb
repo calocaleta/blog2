40ff96f3b1b9e016a57aa4ce7a44346b6b91ab18
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-09-17 21:03:58 -0500

    Creating Article Controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'bienvenida', to: 'home#index'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
 end
